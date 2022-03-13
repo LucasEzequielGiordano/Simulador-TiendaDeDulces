adf45e6d7e52099aa0b384ac2c84d0f040396692
LucasEzequielGiordano <lucasgiordano2219@gmail.com>
2022-03-13 12:56:19 -0300

    agrego carrito

--- js/dom.js
+++ js/dom.js
@@ -2,7 +2,6 @@
 // FUNCION DE INICIO
 function init() {
     elementosEnHTML(dulces);
-    imprimirCarrito()
 }
 
 // IMPRIMO ELEMENTOS DEL ARRAY DULCES EN FORMA DE CARD
@@ -15,47 +14,48 @@ function elementosEnHTML(dulces) {
             <img src="${dulces.images}" class="card-img-top" alt=" ">
             <div class="card-body d-flex flex-column justify-content-center">
                 <h5 class="card-title text-center">${dulces.nombre}</h5>
-                <p class="card-text text-center">$${dulces.precio}</p>
-                <div class="contenedorCantidad">
-                    <button class="btn btn-danger" id="restar${dulces.id}"> - </button>
-                    <p>Cantidad: <span id="cantidad${dulces.id}">0</span></p>
-                    <button class="btn btn-success" id="aumentar${dulces.id}"> + </button>
-                </div>
-                <a href="#" class="btn btn-dark" id="agregar${dulces.id}">Agregar Al Carrito</a>
+                <p class="card-text text-center" id="valor${dulces.precio}">$${dulces.precio}</p>
+                <button href="#" class="btn btn-dark" id="agregar${dulces.id}">Agregar Al Carrito</button>
             </div>
         </div>
         `;
         contenedor.appendChild(card);
-        // EVENTO DE SUMAR PRODUCTOS
-        let aumentar = document.getElementById(`aumentar${dulces.id}`);
-        let cantidad = document.getElementById(`cantidad${dulces.id}`);
-        let contador = 0;
-        aumentar.addEventListener("click", () => {
-            contador++;
-            cantidad.textContent = contador;
-        });
-        // EVENTO DE RESTAR PRODUCTOS
-        let restar = document.getElementById(`restar${dulces.id}`);
-        restar.addEventListener("click", () => {
-            contador--;
-            cantidad.textContent = contador;
-        });
-    });
-}
 
-// FUNCIÓN PARA IMPRIMIR TABLA EN EL CARRITO
-function imprimirCarrito() {
-    let bodyTabla = document.getElementById("bodyTabla");
-    carrito.forEach(dulces => {
-        let info = document.createElement("div");
-        info.innerHTML = `
-        <tr>
-            <td>${dulces.nombre}</td>
-            <td>${dulces.cantidad}</td>
-            <td>$${dulces.precio}</td>
-            <td><button id="eliminar${dulces.id}" type="button" class="btn btn-dark">Eliminar</button></td>
-        </tr>
-        `;
-        bodyTabla.appendChild(info);
+        // EVENTO DE AGREGAR AL CARRITO
+        let agregarAlCarrito = document.getElementById(`agregar${dulces.id}`);
+        agregarAlCarrito.addEventListener("click", () => {
+            carrito.push(new Dulce(dulces.id, dulces.nombre, dulces.precio, dulces.images));
+            // IMPRIMO EN EL CARRITO LOS PRODUCTOS SELECCIONADOS
+            let imprimir = document.getElementById("divCarrito");
+            let listaCarrito = document.createElement("ul");
+            listaCarrito.innerHTML += `
+            <ul>
+                <li>${dulces.nombre}</li>
+                <li><button class="btn btn-danger" id="restar${dulces.id}"> - </button>
+                <span>Cantidad: <span id="cantidad${dulces.id}">1</span></span>
+                <button class="btn btn-success" id="aumentar${dulces.id}"> + </button></li>
+                <li id="valor${dulces.precio}">$${dulces.precio}</li>
+                <li><button class="btn btn-dark">COMPRAR</button></li>
+            </ul>
+            `;
+            imprimir.appendChild(listaCarrito);
+            // EVENTO DE SUMAR PRODUCTOS Y SU VALOR
+            let aumentar = document.getElementById(`aumentar${dulces.id}`);
+            let cantidad = document.getElementById(`cantidad${dulces.id}`);
+            let contador = 0;
+            let valorTotal = document.getElementById(`valor${dulces.precio}`);
+            aumentar.addEventListener("click", () => {
+                contador++;
+                cantidad.textContent = contador;
+                valorTotal.textContent = contador * `${dulces.precio}`;
+            });
+            // EVENTO DE RESTAR PRODUCTOS Y SU VALOR
+            let restar = document.getElementById(`restar${dulces.id}`);
+            restar.addEventListener("click", () => {
+                contador--;
+                cantidad.textContent = contador;
+                valorTotal.textContent = contador * `${dulces.precio}`;
+            });
+        });
     });
 }
