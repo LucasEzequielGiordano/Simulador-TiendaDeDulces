9fd914eefecd8c0fb26641096a815b20f22be3f7
LucasEzequielGiordano <lucasgiordano2219@gmail.com>
2022-03-12 21:20:38 -0300

    agrego eventos de cantidad

--- js/dom.js
+++ js/dom.js
@@ -1,35 +1,61 @@
-// imprimo los elementos
+// DECLARACION DE FUNCIONES
+// FUNCION DE INICIO
+function init() {
+    elementosEnHTML(dulces);
+    imprimirCarrito()
+}
+
+// IMPRIMO ELEMENTOS DEL ARRAY DULCES EN FORMA DE CARD
 function elementosEnHTML(dulces) {
     let contenedor = document.getElementById("contenedor");
     dulces.forEach(dulces => {
-        contenedor.innerHTML += `
+        let card = document.createElement("div");
+        card.innerHTML += `
         <div class="card d-flex .justify-content-center" style="width: 18rem;">
             <img src="${dulces.images}" class="card-img-top" alt=" ">
             <div class="card-body d-flex flex-column justify-content-center">
-                <h5 class="card-title">${dulces.nombre}</h5>
-                <p class="card-text">$${dulces.precio}</p>
+                <h5 class="card-title text-center">${dulces.nombre}</h5>
+                <p class="card-text text-center">$${dulces.precio}</p>
                 <div class="contenedorCantidad">
-                    <button class="btn btn-info"> + </button>
-                    <h6 class="my-5">Cantidad: <span>0</span></h6>
-                    <button class="btn btn-danger"> - </button>
+                    <button class="btn btn-danger" id="restar${dulces.id}"> - </button>
+                    <p>Cantidad: <span id="cantidad${dulces.id}">0</span></p>
+                    <button class="btn btn-success" id="aumentar${dulces.id}"> + </button>
                 </div>
-                <a href="#" class="btn btn-dark">Agregar Al Carrito</a>
+                <a href="#" class="btn btn-dark" id="agregar${dulces.id}">Agregar Al Carrito</a>
             </div>
         </div>
         `;
+        contenedor.appendChild(card);
+        // EVENTO DE SUMAR PRODUCTOS
+        let aumentar = document.getElementById(`aumentar${dulces.id}`);
+        let cantidad = document.getElementById(`cantidad${dulces.id}`);
+        let contador = 0;
+        aumentar.addEventListener("click", () => {
+            contador++;
+            cantidad.textContent = contador;
+        });
+        // EVENTO DE RESTAR PRODUCTOS
+        let restar = document.getElementById(`restar${dulces.id}`);
+        restar.addEventListener("click", () => {
+            contador--;
+            cantidad.textContent = contador;
+        });
     });
 }
 
-// le  pido al usuario que elija un producto del carrito por prompt
-// ese producto lo imprimo debajo de las cards (error en la impresión, se me imprime el numero)
-function datosTomadosPorPrompt() {
-    let carrito = document.querySelector("#carrito");
-    let compraRealizada = prompt(`Elija que dulces va a pedir:\n 0. "${dulces[0].nombre}" $${dulces[0].precio}\n 1. "${dulces[1].nombre}" $${dulces[1].precio}\n 2. "${dulces[2].nombre}" $${dulces[2].precio}\n 3. "${dulces[3].nombre}" $${dulces[3].precio}\n 4. "${dulces[4].nombre}" $${dulces[4].precio}\n 5. "${dulces[5].nombre}" $${dulces[5].precio}\n 6. "${dulces[6].nombre}" $${dulces[6].precio}\n 7. "${dulces[7].nombre}" $${dulces[7].precio}\n 8. "${dulces[8].nombre}" $${dulces[8].precio}\n 9. Irse.`);
-    let p = document.createElement("p");
-    compraRealizada = numeroValidacion(compraRealizada)
-    p.textContent = `Compraste ${compraRealizada}`;
-    carrito.appendChild(p);
+// FUNCIÓN PARA IMPRIMIR TABLA EN EL CARRITO
+function imprimirCarrito() {
+    let bodyTabla = document.getElementById("bodyTabla");
+    carrito.forEach(dulces => {
+        let info = document.createElement("div");
+        info.innerHTML = `
+        <tr>
+            <td>${dulces.nombre}</td>
+            <td>${dulces.cantidad}</td>
+            <td>$${dulces.precio}</td>
+            <td><button id="eliminar${dulces.id}" type="button" class="btn btn-dark">Eliminar</button></td>
+        </tr>
+        `;
+        bodyTabla.appendChild(info);
+    });
 }
-
-elementosEnHTML(dulces);
-setTimeout(datosTomadosPorPrompt, 500);
--- js/index.js
+++ js/index.js
@@ -1,49 +1,54 @@
 /* SIMULADOR - TIENDA DE DULCES
- * 1. Preguntar al usuario que dulces desea comprar
- * 2. Preguntar al usuario si desea otro o quiere retirarse
- * 2.1 En caso de querer seguir, continuar hasta que desee finalizar el proceso
- * 2.2 Sumar el total del precio para sacar el precio final
- * 3. Imprimir en consola lo pedido por el usuario
- * 4. Imprimir en consola todo el array "dulces" ordenado de menor a mayor por precio
+ * 1. Que el usuario pueda elegir que dulce ordenar
+ * 2. Que el usuario pueda elegir la cantidad de dulces a ordenar 
+ * 3. Sumar el total del precio de cada dulce elegido y su cantidad
+ * 4. Calcular el precio final
+ * 5. Imprimir en consola todo el array "dulces" ordenado de menor a mayor por precio
  */
-// declaracion de objetos
+
+// ORDEN DEL CÓDIGO:
+// OBJETOS
+// ARRAYS
+
+// DECLARACION DE OBJETOS
 class Dulce {
-    constructor(nombre, precio, images) {
+    constructor(id, nombre, precio, images) {
+        this.id = id;
         this.nombre = nombre;
         this.precio = precio;
         this.images = images;
     }
 }
 
-// declaracion de arrays
+// DECLARACION DE ARRAYS
 let nuevoDulce;
 const dulces = [];
 let carrito = [];
-dulces.push(new Dulce("Dos Corazones", 80, "./images/Dos-Corazones.jpg"));
-dulces.push(new Dulce("Mogul Gomitas", 60, "./images/Mogul-Gomitas.jpg"));
-dulces.push(new Dulce("Ferrero Rocher", 250, "./images/Ferrero-Rocher.jpg"));
-dulces.push(new Dulce("Shot Bloque Chocolate", 160, "./images/Shot-Bloque-Chocolate.webp"));
-dulces.push(new Dulce("Kinder Chocolate", 255, "./images/Kinder-Chocolate.jpeg"));
-dulces.push(new Dulce("Jorgito Alfajor", 85, "./images/Jorgito-Alfajor.jpg"));
-dulces.push(new Dulce("Milka Chocolate", 310, "./images/Milka-Chocolate.png"));
-dulces.push(new Dulce("Pepitos Alfajor", 150, "./images/Pepitos-Alfajor.jpg"));
-dulces.push(new Dulce("Bon o Bon", 55, "./images/Bon-o-Bon.jpg"));
+dulces.push(new Dulce(0, "Dos Corazones", 80, "./images/Dos-Corazones.jpg"));
+dulces.push(new Dulce(1, "Mogul Gomitas", 60, "./images/Mogul-Gomitas.jpg"));
+dulces.push(new Dulce(2, "Ferrero Rocher", 250, "./images/Ferrero-Rocher.jpg"));
+dulces.push(new Dulce(3, "Shot Bloque Chocolate", 160, "./images/Shot-Bloque-Chocolate.webp"));
+dulces.push(new Dulce(4, "Kinder Chocolate", 255, "./images/Kinder-Chocolate.jpeg"));
+dulces.push(new Dulce(5, "Jorgito Alfajor", 85, "./images/Jorgito-Alfajor.jpg"));
+dulces.push(new Dulce(6, "Milka Chocolate", 310, "./images/Milka-Chocolate.png"));
+dulces.push(new Dulce(7, "Pepitos Alfajor", 150, "./images/Pepitos-Alfajor.jpg"));
+dulces.push(new Dulce(8, "Bon o Bon", 55, "./images/Bon-o-Bon.jpg"));
 
 // inicio del simulador
 // validacion de la eleccion del cliente
-function numeroValidacion(numero) {
-    while (numero < 0 || numero > 9 || isNaN(numero)) {
-        numero = parseInt(prompt(`Número invalido, Elija que dulces va a pedir:\n 0. "${dulces[0].nombre}" $${dulces[0].precio}\n 1. "${dulces[1].nombre}" $${dulces[1].precio}\n 2. "${dulces[2].nombre}" $${dulces[2].precio}\n 3. "${dulces[3].nombre}" $${dulces[3].precio}\n 4. "${dulces[4].nombre}" $${dulces[4].precio}\n 5. "${dulces[5].nombre}" $${dulces[5].precio}\n 6. "${dulces[6].nombre}" $${dulces[6].precio}\n 7. "${dulces[7].nombre}" $${dulces[7].precio}\n 8. "${dulces[8].nombre}" $${dulces[8].precio}\n 9. Irse.`));
-    }
-    return numero;
-}
+// function numeroValidacion(numero) {
+//     while (numero < 0 || numero > 9 || isNaN(numero)) {
+//         numero = parseInt(prompt(`Número invalido, Elija que dulces va a pedir:\n 0. "${dulces[0].nombre}" $${dulces[0].precio}\n 1. "${dulces[1].nombre}" $${dulces[1].precio}\n 2. "${dulces[2].nombre}" $${dulces[2].precio}\n 3. "${dulces[3].nombre}" $${dulces[3].precio}\n 4. "${dulces[4].nombre}" $${dulces[4].precio}\n 5. "${dulces[5].nombre}" $${dulces[5].precio}\n 6. "${dulces[6].nombre}" $${dulces[6].precio}\n 7. "${dulces[7].nombre}" $${dulces[7].precio}\n 8. "${dulces[8].nombre}" $${dulces[8].precio}\n 9. Irse.`));
+//     }
+//     return numero;
+// }
 
 // almacena en el carrito el dulce elegido
-function ordenDeCompra(eleccion) {
-    carrito.push(dulces[eleccion]);
-    alert(`Se agregó al carrito el dulces "${dulces[eleccion].nombre}"`);
-    console.table(carrito);
-}
+// function ordenDeCompra(eleccion) {
+//     carrito.push(dulces[eleccion]);
+//     alert(`Se agregó al carrito el dulces "${dulces[eleccion].nombre}"`);
+//     console.table(carrito);
+// }
 
 // opcion de continuar comprando o irse
 // en caso de continuar, sigue pusheando al carrito
@@ -54,11 +59,11 @@ function ordenDeCompra(eleccion) {
 // }
 
 // se calcula el total de todo lo pedido por el cliente
-function calcularPrecioFinal() {
-    let precioFinal = 0;
-    carrito.forEach((Element) => precioFinal += Element.precio);
-    return precioFinal;
-}
+// function calcularPrecioFinal() {
+//     let precioFinal = 0;
+//     carrito.forEach((Element) => precioFinal += Element.precio);
+//     return precioFinal;
+// }
 
 // invocacion de funciones
 // let respuesta = true;
