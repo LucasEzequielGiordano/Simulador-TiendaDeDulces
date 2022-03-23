aee4e1fbb9c9713461a616c81451ac8f08d45081
LucasEzequielGiordano <lucasgiordano2219@gmail.com>
2022-03-22 23:02:11 -0300

    re organizo el codigo

--- js/dom.js
+++ js/dom.js
@@ -1,161 +1,129 @@
 // Declaracion de funciones
-// Funcion donde recorro el array dulces y lo imprimo en cards
-function elementosEnHTML(dulces) {
-    let contenedor = document.getElementById("contenedor");
-    dulces.forEach(dulces => {
-        let card = document.createElement("div");
-        card.innerHTML = `
+// Recorro el array "dulces" e imprimo las cards 
+function crearStringCatalogo() {
+    let string = ""
+    for (const dulce of dulces) {
+        string += `
         <div class="card d-flex .justify-content-center" style="width: 18rem;">
-            <img src="${dulces.images}" class="card-img-top" alt=" ">
+            <img src="${dulce.images}" class="card-img-top" alt=" ">
             <div class="card-body d-flex flex-column justify-content-center">
-                <h5 class="card-title text-center">${dulces.nombre}</h5>
-                <p class="card-text text-center" id="valor${dulces.precio}">$${dulces.precio}</p>
-                <button href="#" class="btn btn-dark" id="agregar${dulces.id}">Agregar Al Carrito</button>
+                <h5 class="card-title text-center">${dulce.nombre}</h5>
+                <p class="card-text text-center" id="valor${dulce.precio}">$${dulce.precio}</p>
+                <button href="#" class="btn btn-dark" id="agregar${dulce.id}">Agregar Al Carrito</button>
             </div>
         </div>
-        `;
-        contenedor.appendChild(card);
-
-        // Evento de agregar card al carrito
-        // Al hacer click sobre el boton, pusheo al carrito 
-        // Mando al localStorage el objeto seleccionado
-        // Luego imprimo en el carrito los productos seleccionados
-        let agregarAlCarrito = document.getElementById(`agregar${dulces.id}`);
-        agregarAlCarrito.addEventListener("click", () => {
-            // Validación de código
-            // Busco en el id de cada producto seleccionado
-            let idProducto = dulces.id
-            let dulceEnCarrito = carrito.find((elemento) => {
-                if (elemento.id == idProducto) {
-                    return true;
-                }
-            });
-            if (dulceEnCarrito) {
-                let index = carrito.findIndex((elemento) => {
-                    if (elemento.id === dulceEnCarrito.id) {
-                        return true;
-                    }
-                });
-                carrito[index].aumentar;
-            } else {
-                carrito.push(new Dulce(dulces.id, dulces.nombre, dulces.precio, dulces.images, 1));
-            }
-            localStorage.setItem("carritoEnStorage", JSON.stringify(carrito));
-            let imprimir = document.getElementById("divCarrito");
-            let listaCarrito = document.createElement("ul");
-            listaCarrito.innerHTML = `
-            <li>${dulces.nombre}</li>
-            <li><button class="btn btn-danger" id="restar${dulces.id}"> - </button>
-            <span>Cantidad: <span id="cantidad${dulces.id}">1</span></span>
-            <button class="btn btn-success" id="aumentar${dulces.id}"> + </button></li>
-            <li id="valorTotal${dulces.precio}">$${dulces.precio}</li>
-            <li><button class="btn btn-danger" id="eliminar${dulces.id}">ELIMINAR</button></li>
-            `;
-            imprimir.appendChild(listaCarrito);
-
-            // Evento de sumar productos y su valor
-            // Tomo el valor de la cantidad "1" del dulce seleccionado
-            // Le sumo el precio a medida que aumente la cantidad
-            let aumentar = document.getElementById(`aumentar${dulces.id}`);
-            let cantidad = document.getElementById(`cantidad${dulces.id}`);
-            let contador = 0;
-            let valorTotal = document.getElementById(`valorTotal${dulces.precio}`);
-            aumentar.addEventListener("click", () => {
-                contador++;
-                cantidad.textContent = contador;
-                valorTotal.textContent = contador * `${dulces.precio}`;
-            });
-
-            // Evento de restar productos y su valor
-            // Al restar la cantidad, se resta el precio 
-            // Al llegar a 0 la cantidad, se elimina el objeto del carrito
-            let restar = document.getElementById(`restar${dulces.id}`);
-            restar.addEventListener("click", () => {
-                if (contador <= 0) {
-                    imprimir.removeChild(listaCarrito);
-                    borrarElementoStorage();
-                } else {
-                    contador--;
-                    cantidad.textContent = contador;
-                    valorTotal.textContent = contador * `${dulces.precio}`;
-                }
-            });
-
-            // Evento de eliminar productos del carrito
-            // Al haccer click sobre el botón rojo
-            // Se eliminará el producto del carrito
-            let eliminarObjetoDeLista = document.getElementById(`eliminar${dulces.id}`);
-            eliminarObjetoDeLista.addEventListener("click", () => {
-                imprimir.removeChild(listaCarrito);
-                borrarElementoStorage();
-            });
-        });
-    });
-}
-
-// Funcion para eliminar el elemento seleccionado del storage
-// Se lo borra y luego se vuelve a guardar en el storage sin este
-function borrarElementoStorage() {
-    let carritoStorage = JSON.parse(localStorage.getItem("carritoEnStorage"));
-    let indexStorage = carritoStorage.findIndex(element => element.id === dulces.id);
-    carritoStorage.splice(indexStorage, 1);
-    let reingresoStorage = JSON.stringify(carritoStorage);
-    localStorage.setItem("carritoEnStorage", reingresoStorage)
-}
-
-// Función para cargar el carrito desde el localStorage
-// En caso que no haya nada "Null" retoranará un array vacío
-// Sino retornará el listado carrito
-function cargarListado() {
-    let listadoCarrito = JSON.parse(localStorage.getItem("carritoEnStorage"));
-    if (listadoCarrito == null) {
-        return [];
+        `
     }
-    listadoCarrito.forEach((dulces) => {
-        let imprimir = document.getElementById("divCarrito");
-        let listaCarrito = document.createElement("ul");
-        listaCarrito.innerHTML = `
-    <li>${dulces.nombre}</li>
-    <li><button class="btn btn-danger" id="restar${dulces.id}"> - </button>
-    <span>Cantidad: <span id="cantidad${dulces.id}">${dulces.cantidad}</span></span>
-    <button class="btn btn-success" id="aumentar${dulces.id}"> + </button></li>
-    <li id="valorTotal${dulces.precio}">$${dulces.precio}</li>
-    <li><button class="btn btn-danger" id="eliminar${dulces.id}">ELIMINAR</button></li>
-    `;
-        imprimir.appendChild(listaCarrito);
+    return string;
+}
 
-        // Reutilizo los botones de eventos
-        let aumentar = document.getElementById(`aumentar${dulces.id}`);
-        let cantidad = document.getElementById(`cantidad${dulces.id}`);
-        let contador = 0;
-        let valorTotal = document.getElementById(`valorTotal${dulces.precio}`);
+// Recorro el array "listaCarrito" e imprimo los valores del dulce
+function crearStringCarrito(listaCarrito) {
+    let string = ""
+    for (const dulce of listaCarrito) {
+        string += `
+        <ul>
+        <li scope="row">${dulce.nombre}</li>
+        <li scope="row"><button class="btn btn-danger" id="restar${dulce.id}"> - </button>
+        <span>Cantidad: <span id="cantidad${dulce.id}">1</span></span>
+        <button class="btn btn-success" id="aumentar${dulce.id}"> + </button></li>
+        <li scope="row" id="valorTotal${dulce.precio}">$${dulce.precio}</li>
+        <li scope="row" ><button class="btn btn-danger" id="eliminar${dulce.id}">ELIMINAR</button></li>
+        </ul>
+        `
+    }
+    return string
+}
+
+// Funcion para mostrar las cards con los "dulces"
+function catalogoDulces() {
+    let contenedor = document.getElementById("contenedor");
+    let card = document.createElement("div");
+    card.innerHTML = crearStringCatalogo();
+    contenedor.appendChild(card);
+    agregarAlCarrito()
+    imprimirDivCarrito()
+}
+
+// Funcion para imprimir el carrito
+function imprimirDivCarrito() {
+    let imprimir = document.getElementById("divCarrito");
+    imprimir.textContent = ""
+    let listaCarrito = document.createElement("div");
+    listaCarrito.innerHTML = crearStringCarrito(obtenerDatosStorage())
+    imprimir.appendChild(listaCarrito);
+    agregarAlCarrito()
+    operacionesCarrito()
+    eliminarDelCarrito()
+}
+
+// Declaro un evento "Click" para sumar unidades de dulces al carrito
+function agregarAlCarrito() {
+    for (const dulce of dulces) {
+        let agregarAlCarrito = document.getElementById(`agregar${dulce.id}`);
+        agregarAlCarrito.addEventListener("click", () => {
+            let obtenerDatos = obtenerDatosStorage()
+            if (buscarDulceStorage(dulce.id, obtenerDatos)) {} else {
+                obtenerDatos.push(dulce)
+                modificarDatosStorage(obtenerDatos)
+                imprimirDivCarrito()
+                operacionesCarrito()
+                eliminarDelCarrito()
+            }
+        })
+    }
+}
+
+// Declaro las operaciones de cantidad en el carrito
+function operacionesCarrito() {
+    let listaCarrito = obtenerDatosStorage()
+    // Recorro el array  "listaCarrito" y por cada evento "click"
+    // Sumo una unidad a la cantidad del dulce en carrito
+    for (const dulce of listaCarrito) {
+        let aumentar = document.getElementById(`aumentar${dulce.id}`);
+        let cantidad = document.getElementById(`cantidad${dulce.id}`);
+        let valorTotal = document.getElementById(`valorTotal${dulce.precio}`);
         aumentar.addEventListener("click", () => {
-            contador++;
-            cantidad.textContent = contador;
-            valorTotal.textContent = contador * `${dulces.precio}`;
+            agregarUnidadDulce(dulce)
+            actualizarPrecioTotalDulce(dulce)
+            modificarDatosStorage(listaCarrito)
+            cantidad.textContent = dulce.cantidad
+            valorTotal.textContent = dulce.precioTotal
         });
+    }
 
-        let restar = document.getElementById(`restar${dulces.id}`);
+    // Recorro el array  "listaCarrito" y por cada evento "click"
+    // Resto una unidad a la cantidad del dulce en carrito
+    for (const dulce of listaCarrito) {
+        let restar = document.getElementById(`restar${dulce.id}`);
+        let cantidad = document.getElementById(`cantidad${dulce.id}`);
+        let valorTotal = document.getElementById(`valorTotal${dulce.precio}`);
         restar.addEventListener("click", () => {
-            if (contador <= 0) {
-                imprimir.removeChild(listaCarrito);
-                borrarElementoStorage();
+            if (dulce.cantidad == 1) {
+                borrarDulceStorage(dulce.id, listaCarrito);
+                imprimirDivCarrito()
             } else {
-                contador--;
-                cantidad.textContent = contador;
-                valorTotal.textContent = contador * `${dulces.precio}`;
+                quitarUnidadDulce(dulce)
+                actualizarPrecioTotalDulce(dulce)
+                modificarDatosStorage(listaCarrito)
+                cantidad.textContent = dulce.cantidad
+                valorTotal.textContent = dulce.precioTotal
             }
         });
+    }
+}
 
-        let eliminarObjetoDeLista = document.getElementById(`eliminar${dulces.id}`);
+// Elimino del carrito y del storage el dulce seleccionado
+function eliminarDelCarrito() {
+    let listaCarrito = obtenerDatosStorage()
+    for (const dulce of listaCarrito) {
+        let eliminarObjetoDeLista = document.getElementById(`eliminar${dulce.id}`);
         eliminarObjetoDeLista.addEventListener("click", () => {
-            imprimir.removeChild(listaCarrito);
-            borrarElementoStorage();
+            borrarDulceStorage(dulce.id, listaCarrito);
+            imprimirDivCarrito()
         });
-    });
-    return listadoCarrito;
+    }
 }
 
 // INVOCACION DE FUNCIONES
-carrito = cargarListado();
-elementosEnHTML(dulces);
+catalogoDulces()
---
+++ js/main.js
@@ -0,0 +1,74 @@
+// Declaracion de objetos
+class Dulce {
+    constructor(id, nombre, precio, images) {
+        this.id = id;
+        this.nombre = nombre;
+        this.precio = precio;
+        this.images = images;
+        this.cantidad = 1;
+        this.precioTotal = this.precio;
+    }
+}
+
+// Declaracion de arrays
+let dulces = []
+// Push  a los arrays
+dulces.push(new Dulce(0, "Dos Corazones", 80, "./images/Dos-Corazones.jpg"));
+dulces.push(new Dulce(1, "Mogul Gomitas", 60, "./images/Mogul-Gomitas.jpg"));
+dulces.push(new Dulce(2, "Ferrero Rocher", 250, "./images/Ferrero-Rocher.jpg"));
+dulces.push(new Dulce(3, "Shot Bloque Chocolate", 160, "./images/Shot-Bloque-Chocolate.webp"));
+dulces.push(new Dulce(4, "Kinder Chocolate", 255, "./images/Kinder-Chocolate.jpeg"));
+dulces.push(new Dulce(5, "Jorgito Alfajor", 85, "./images/Jorgito-Alfajor.jpg"));
+dulces.push(new Dulce(6, "Milka Chocolate", 310, "./images/Milka-Chocolate.png"));
+dulces.push(new Dulce(7, "Pepitos Alfajor", 150, "./images/Pepitos-Alfajor.jpg"));
+dulces.push(new Dulce(8, "Bon o Bon", 55, "./images/Bon-o-Bon.jpg"));
+
+// Declaracion de metodos
+// Funcion para agregar una unidad en la cantidad del dulce
+function agregarUnidadDulce(dulce) {
+    dulce.cantidad = dulce.cantidad + 1;
+}
+
+// Funcion para quitar una unidad en la cantidad del dulce
+function quitarUnidadDulce(dulce) {
+    dulce.cantidad = dulce.cantidad - 1;
+}
+
+// Funcion para actualizar el precio total del dulce
+function actualizarPrecioTotalDulce(dulce) {
+    dulce.precioTotal = dulce.precio * dulce.cantidad;
+}
+
+// Recibo los datos almacenados en storage
+// Devuelvo el listado
+function obtenerDatosStorage() {
+    let listadoCarrito = JSON.parse(localStorage.getItem("carritoEnStorage"));
+    if (listadoCarrito == null) {
+        return [];
+    }
+    return listadoCarrito;
+}
+
+// Recibo la nueva lista a modificar y la meto en el carrito
+function modificarDatosStorage(listaCarrito) {
+    localStorage.setItem("carritoEnStorage", JSON.stringify(listaCarrito));
+}
+
+// Recibo el ID de un dulce y una lista
+// Devuelvo boolean si encuentra ese ID en la lista
+function buscarDulceStorage(id, listaCarrito) {
+    for (const dulce of listaCarrito) {
+        if(dulce.id == id) {
+            return true;
+        }
+    }
+    return false;
+}
+
+// Recibo el ID de un dulce y una lista
+// Para borrar el dulce en la lista y actualizar el storage
+function borrarDulceStorage(id, listaCarrito) {
+    let indexStorage = listaCarrito.findIndex(element => element.id === id);
+    listaCarrito.splice(indexStorage, 1);
+    modificarDatosStorage(listaCarrito);
+}
